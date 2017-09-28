d033ee6c9dbc60c8bb35d511f57e601801c19524
hungmuha <hung.soboss@gmail.com>
2017-09-28 13:33:31 -0600

    getting the portfolio done

--- HTML/contact.php
+++ HTML/contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'hungmuha712@gmail.com';
+$siteTitle = 'hung muhamath portfolio';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
